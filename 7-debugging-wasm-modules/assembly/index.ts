3b20f049e1883aeda419cb6804d0c6e0bee92063
yenilikci <m.melihcelik017@outlook.com>
2023-07-02 13:59:40 +0300

    lesson 7

---
+++ 7-debugging-wasm-modules/assembly/index.ts
@@ -0,0 +1,9 @@
+declare function log(n: i32): void
+
+export function minusOne(n: i32): i32 {
+  if (n === 44) {
+    log(n);
+  }
+
+  return n - 1;
+}
---
+++ 7-debugging-wasm-modules/index.js
@@ -0,0 +1,2 @@
+import { minusOne } from "./build/release.js";
+console.log(minusOne(2));
